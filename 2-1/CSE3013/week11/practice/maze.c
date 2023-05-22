55485d877134ae08ad8aab645cfea548004f886b
EGU1832 <egu1832@sogang.ac.kr>
2023-05-22 16:45:30 +0900

    2023.05.22 updated

--- 2-1/CSE3013/week11/practice/maze.c
+++ 2-1/CSE3013/week11/practice/maze.c
@@ -1,6 +1,7 @@
 #include "maze.h"
 
 int main() {
+	srand(time(NULL));
     FILE *file;
 
     //open file
@@ -17,15 +18,10 @@ int main() {
 	scanf("%d", &N);
 	scanf("%d", &M);
 
-	makeMaze(N, M, &field);
+	makeBasicMaze(&field, &set);
+	makeMaze();
 
-	//print maze
-	for (int i = 0; i < HEIGHT; i++){
-		for (int j = 0; j < WIDTH; j++) {
-			printf("%c", field[i][j]);
-		}
-		printf("\n");
-	}
+	print();
 
 	//free allocated memory at field
 	for (int i = 0; i < HEIGHT; i++) {
@@ -33,13 +29,18 @@ int main() {
     }
     free(field);
 
+	for (int i = 0; i < M; i++) {
+        free(set[i]);
+    }
+    free(set);
+
     //close file
     fclose(file);
 
     return 0;
 }
 
-void makeMaze(int N, int M, char*** field) {
+void makeBasicMaze(char*** field, int*** set) {
 
 	HEIGHT = M * 2 + 1;
 	WIDTH = N * 2 + 1;
@@ -61,6 +62,24 @@ void makeMaze(int N, int M, char*** field) {
 		*/
     }
 
+	*set = (int**)malloc(M * sizeof(int*));
+    /*
+	if (*set == NULL) {
+        printf("memory allocation error!");
+        return 1;
+    }
+	*/
+    for (int i = 0; i < M; i++) {
+        (*set)[i] = (int*)malloc(N * sizeof(int));
+		/*
+        if ((*set)[i] == NULL) {
+            printf("memory allocation error!");
+            return 1;
+        }
+		*/
+    }
+	setNum = 1;
+
 	//make maze field with form
 	for (int i = 0; i < HEIGHT; i++) {
 		if (i % 2 == 0) {
@@ -77,4 +96,138 @@ void makeMaze(int N, int M, char*** field) {
 		}
 	}
 
+}
+
+void makeMaze() {
+	
+	for (int i = 0; i < HEIGHT; i++) {
+		if (i == 1) { determineSet(i); }
+		if (i % 2 == 1 && i != HEIGHT - 2){
+			determineRow(i);
+			determineColumn(i+1);
+			determineSet(i + 2);
+			print();
+		}
+		else if (i == HEIGHT - 2) { 
+			determineLastRow(i);
+		}
+	}
+
+}
+
+void determineRow(int currHEIGHT) {
+
+	int setM = currHEIGHT / 2;
+
+	for (int setN = 1; setN < N; setN++) {
+		int currWIDTH = setN * 2 + 1;
+		//if two adjacent rooms belong to a different set
+		if (set[setM][setN] != set[setM][setN - 1]) {
+			//choose rather delete '|' or not
+			int choice = rand() % 2;
+			if (choice == 0) { 
+				//delete '|'
+				field[currHEIGHT][currWIDTH - 1] = ' ';
+
+				//putting all connected rooms together in the same set
+				int min = findMin(set[setM][setN] , set[setM][setN - 1]);
+				int max = findMax(set[setM][setN] , set[setM][setN - 1]);
+
+				for (int j = 0; j < M; j++) {
+					for (int k = 0; k < N; k++) {
+						if (set[j][k] == max) { set[j][k] = min; }
+					}
+				}
+			}
+		}
+	}
+
+}
+
+void determineLastRow(int currM) {
+
+	for (int i = 1; i < N; i++) {
+		if (field[currM][i * 2 + 1 - 1] == '|' && set[currM / 2][i] != set[currM / 2][i - 1]) {
+			field[currM][i * 2 + 1 - 1] = ' ';
+
+			int min = findMin(set[currM / 2][i], set[currM / 2][i - 1]);
+			int max = findMax(set[currM / 2][i], set[currM / 2][i - 1]);
+			set[currM / 2][i] = set[currM / 2][i - 1];
+
+			for (int j = 0; j < M; j++) {
+				for (int k = 0; k < N; k++) {
+					if (set[j][k] == max) { set[j][k] = min; }
+				}
+			}
+		}
+	}
+
+}
+
+void determineColumn(int currHEIGHT) {
+
+	int flag = 0;
+	int setM = currHEIGHT / 2;
+
+	for (int i = 0; i < N; i++) {
+		//if two adjacent rooms belong to a same set
+		if (set[setM - 1][i] == set[setM - 1][i + 1]) {
+			//choose rather delete '-' or not
+			int choice = rand() % 2;
+			if (choice == 0) {
+				field[currHEIGHT][i * 2 + 1] = ' ';
+				set[setM][i] = set[setM - 1][i];
+				flag = 1;
+			}
+		}
+
+		//if two adjacent rooms belongs to a defferent set
+		else { 
+			//if field didn't changed until
+			if (flag == 0) {
+				field[currHEIGHT][i * 2 + 1] = ' ';
+				set[setM][i] = set[setM - 1][i];
+			}
+			//if field changed until
+			flag = 0;
+		}
+	}
+
+}
+
+void determineSet(int currM) {
+
+	for (int i = 0; i < N; i++) {
+		if (set[currM / 2][i] == 0) {
+			set[currM / 2][i] = setNum++;
+		}
+	}
+
+}
+
+void print() {
+
+	//print maze
+	for (int i = 0; i < HEIGHT; i++){
+		for (int j = 0; j < WIDTH; j++) {
+			printf("%c", field[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < M; i++){
+		for (int j = 0; j < N; j++) {
+			printf("%d ", set[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+
+int findMin(int a, int b) {
+	return (a < b) ? a : b;
+}
+
+int findMax(int a, int b) {
+    return (a > b) ? a : b;
 }
--- 2-1/CSE3013/week11/practice/maze.h
+++ 2-1/CSE3013/week11/practice/maze.h
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int N, M;	//N: WIDTH, M: HEIGHT
-int WIDTH, HEIGHT;
-char** field;
+int N, M;			//N: WIDTH, M: HEIGHT
+int WIDTH, HEIGHT;	//폭과 높이
+char** field;		//maze를 나타내는 이차원 배열
+int** set;			//칸이 속해있는 집합을 나타내는 배열
+int setNum;
 
-void makeMaze(int N, int M, char*** field);
+void makeBasicMaze(char*** field, int*** set);
+void makeMaze();
+void determineRow(int currM);
+void determineLastRow(int currM);
+void determineColumn(int currM);
+void determineSet(int currM);
+void print();
+int findMin(int a, int b);
+int findMax(int a, int b);
+// N,M 칸은 field[N * 2 + 1][M * 2 + 1]에 저장되어있음
